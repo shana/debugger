27324ca239fb2649dc940c07da59f9c7db19e570
Rodrigo B. de Oliveira <rodrigo@unity3d.com>
2012-07-17 14:55:00 -0300

    [CodeEditor] code gardening

--- src/CodeEditor.Debugger.Unity.Engine/ExecutingLineAdornment.cs
+++ src/CodeEditor.Debugger.Unity.Engine/ExecutingLineAdornment.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using CodeEditor.Composition;
+﻿using CodeEditor.Composition;
 using CodeEditor.Text.UI.Unity.Engine;
-using Mono.Debugger.Soft;
 using UnityEngine;
 
 namespace CodeEditor.Debugger.Unity.Engine
@@ -9,30 +7,26 @@ namespace CodeEditor.Debugger.Unity.Engine
 	[Export(typeof(ITextViewAdornment))]
 	class ExecutingLineAdornment : ITextViewAdornment
 	{
-		private Texture2D _texture;
-		private GUIStyle _text;
+		private readonly Texture2D _texture;
 
 		[Import]
-		private ExecutingLineProvider ExecutingLineProvider { get; set; }
+		ExecutingLineProvider ExecutingLineProvider { get; set; }
 
 		public ExecutingLineAdornment()
 		{
 			_texture = new Texture2D(1, 1);
-			_texture.SetPixel(0,0,new Color(1f,.5f,.5f,.5f));
+			_texture.SetPixel(0, 0, new Color(1f, .5f, .5f, .5f));
 		}
 
 		public void Draw(ITextViewLine line, Rect lineRect)
 		{
-			Console.WriteLine("line.LineNumber = "+line.LineNumber);
-			Console.WriteLine("ExecutingLineProvider.LineNumber = " + ExecutingLineProvider.LineNumber);
-			
 			if (line.LineNumber == ExecutingLineProvider.LineNumber)
 				Draw(lineRect);
 		}
 
 		private void Draw(Rect lineRect)
 		{
-			GUI.DrawTexture(lineRect,_texture, ScaleMode.StretchToFill, false);
+			GUI.DrawTexture(lineRect, _texture, ScaleMode.StretchToFill, false);
 		}
 	}
 }
