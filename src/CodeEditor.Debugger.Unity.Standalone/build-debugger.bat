9e780ae90112ec3a94a9762fd3da0c2cd1f38794
Lucas Meijer <lucas@unity3d.com>
2012-07-17 13:42:00 +0200

    version .meta files in the debuggerproject so dll's keep the same guids over time.

--- src/CodeEditor.Debugger.Unity.Standalone/build-debugger.bat
+++ src/CodeEditor.Debugger.Unity.Standalone/build-debugger.bat
@@ -1,10 +1,11 @@
 @echo off
-SET PROJECTPATH="UnityProject\Project"
-SET BUILDPATH="UnityProject\Build\Windows"
-SET BIN="%PROJECTPATH%\Assets\bin"
+SET PROJECTPATH=UnityProject\Project
+SET BUILDPATH=UnityProject\Build\Windows
+SET BIN=%PROJECTPATH%\Assets\bin
 
 rd /s /q %BIN%
 mkdir %BIN%
+echo copying to %BIN%
 xcopy bin\Debug\*.dll %BIN%
 del %BIN%\UnityEngine.dll
 
@@ -26,10 +27,10 @@ set ABS_BUILDPATH=%CD%
 popd
 
 
-..\..\..\..\build\WindowsEditor\Unity.exe -batchmode -projectpath %ABS_PROJECTPATH% -buildWindowsPlayer %ABS_BUILDPATH%\Debugger.exe -quit
+..\..\..\..\build\WindowsEditor\Unity.exe -batchmode -projectpath "%ABS_PROJECTPATH%" -buildWindowsPlayer "%ABS_BUILDPATH%\Debugger.exe" -quit
 
 del %BUILDPATH%\Debugger_Data\Managed\CodeEditor.*
 del %BUILDPATH%\Debugger_Data\Managed\Mono.Debugger.Soft.dll
 del %BUILDPATH%\Debugger_Data\Managed\Mono.Cecil.dll
 
-rd /s /q %BIN%
+del %BIN%\*.dll
