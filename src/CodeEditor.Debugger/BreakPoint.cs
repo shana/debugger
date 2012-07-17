85862e05e90488355fc3f0175dcb8b732fba48be
Lucas Meijer <lucas@unity3d.com>
2012-07-17 23:07:00 +0200

    make breakpoints live in a specific file.

--- src/CodeEditor.Debugger/BreakPoint.cs
+++ src/CodeEditor.Debugger/BreakPoint.cs
@@ -2,9 +2,19 @@ namespace CodeEditor.Debugger
 {
 	public interface IBreakPoint
 	{
+		string File { get; }
+		int LineNumber { get; }
 	}
 
 	class BreakPoint : IBreakPoint
 	{
+		public string File { get; private set; }
+		public int LineNumber { get; private set; }
+
+		public BreakPoint(string fileName, int lineNumber)
+		{
+			File = fileName;
+			LineNumber = lineNumber;
+		}
 	}
 }
--- src/CodeEditor.Debugger/DebugBreakPointProvider.cs
+++ src/CodeEditor.Debugger/DebugBreakPointProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using CodeEditor.Composition;
 using Mono.Debugger.Soft;
 
@@ -9,27 +10,26 @@ namespace CodeEditor.Debugger
 	public interface IDebugBreakPointProvider
 	{
 		IBreakPoint GetBreakPointAt(string file, int lineNumber);
-		void ToggleBreakPointAt(string fullName, int lineNumber);
+		void ToggleBreakPointAt(string fileName, int lineNumber);
 	}
 
 	[Export(typeof(IDebugBreakPointProvider))]
 	class DebugBreakPointProvider : IDebugBreakPointProvider
 	{
-		private readonly IBreakPoint _breakPoint = new BreakPoint();
-
-		readonly List<int> _setBreakPoints = new List<int>();
+		readonly List<IBreakPoint> _breakPoints = new List<IBreakPoint>();
 
 		public IBreakPoint GetBreakPointAt(string file, int lineNumber)
 		{
-			return (_setBreakPoints.Contains(lineNumber) ? _breakPoint : null);
+			return _breakPoints.FirstOrDefault(bp => bp.File == file && bp.LineNumber == lineNumber);
 		}
 
-		public void ToggleBreakPointAt(string fullName, int lineNumber)
+		public void ToggleBreakPointAt(string fileName, int lineNumber)
 		{
-			if (GetBreakPointAt(fullName, lineNumber) == null)
-				_setBreakPoints.Add(lineNumber);
+			var breakPoint = GetBreakPointAt(fileName, lineNumber);
+			if (breakPoint == null)
+				_breakPoints.Add(new BreakPoint(fileName, lineNumber));
 			else
-				_setBreakPoints.Remove(lineNumber);
+				_breakPoints.Remove(breakPoint);
 		}
 	}
 }
